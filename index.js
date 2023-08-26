98e19a4d153b60457f34795b7b8d3c7a76e71c25
Manasa G <manasa.g7890@hotmail.com>
2023-08-26 20:17:03 +0530

    fetched(index)

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
-
+app.use(express.urlencoded({extended:true}))
 app.use('/public', express.static(`${process.cwd()}/public`));
 
 app.get('/', function(req, res) {
@@ -18,6 +18,47 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+const originaUrls = [];
+const shortUrls= [];
+
+app.post('/api/shorturl',(req,res)=>{
+
+  const url = req.body.url;
+  const foundIndex= originaUrls.indexOf(url)
+
+  if(!url.includes("https://") && !url.includes("http://")){
+    return res.json({
+       error: 'invalid url' 
+    })
+  }
+
+  if(foundIndex<0){
+    originaUrls.push(url)
+    shortUrls.push(shortUrls.length)
+    return res.json({
+      original_url : url ,
+      short_url: shortUrls.length-1
+    })
+  }
+  return res.json({
+    original_url:url,
+    short_url:shortUrls[foundIndex]
+  })
+  
+})
+
+app.get("/api/shorturl/:shorturl",(req,res)=>{
+  const shorturl =parseInt(req.params.shorturl)
+  const found = shortUrls.indexOf(shorturl)
+
+  if(found<0){
+    return res.json({
+      error:'not a valid short url'
+    })
+  }
+   res.redirect(originaUrls[found])
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
